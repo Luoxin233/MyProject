6014c68c08044772085ac18c74443879173f42a5
Luoxin233 <rrwuyule@gmail.com>
2021-06-09 16:36:25 +0800

    加入测试项目

---
+++ NewProjectCpp/Source/NewProjectCpp/MyActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE(FDelegateName);
+
+UCLASS()
+class NEWPROJECTCPP_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+		void ExecuteTask(const FDelegateName& DelegateName);
+
+	UFUNCTION(BlueprintCallable)
+		void ExecTest();
+
+public:
+	FDelegateName DelegateTest;
+};
---
+++ NewProjectCpp/Source/NewProjectCpp/NewProjectCppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewProjectCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWPROJECTCPP_API ANewProjectCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
